ad5873b3877ad903b7d591fdbcf41f19ba599087
faznaa <faznaaharees@gmail.com>
2024-09-13 18:05:27 +0530

    added dockerfile

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS,cross_origin
-from bson.objectid import ObjectId
-import random
-import requests
 from setup import AMADEUS_API_KEY, AMADEUS_API_SECRET
-from functions import getFlightsData, getCheapestFlight
+from functions import  getCheapestFlight
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -23,19 +20,7 @@ def getFlights():
     origin = args.get('origin')
     destination = args.get('destination')
     date = args.get('date')
-    print(origin,destination,date)
-    data = getFlightsData(origin,destination,date)
-    if(data):
-        cheapest = getCheapestFlight(data)
-        price =int(float(cheapest['price']['total']))
-        return {
-        "data": {
-        "origin": origin,
-        "destination": destination,
-        "departure_date": date,
-        "price": f"{price} USD"
-        }
-        }
-        # return data
-    return "No data"
+    nocache = args.get('nocache') == '1'
+    data = getCheapestFlight(origin,destination,date,nocache)
+    return data
     
--- functions.py
+++ functions.py
@@ -1,11 +1,17 @@
 import requests
 import time
 from setup import AMADEUS_API_KEY, AMADEUS_API_SECRET
+import redis
+import json
+
+redisClient = redis.Redis(host='redis', port=6379, decode_responses=True)
+
 access_token = None
-token_expiry = None
+expiry = None
+CACHE_EXPIRY = 10*60
 
 def getAccessToken():
-    global access_token, token_expiry
+    global access_token, expiry
     url = "https://test.api.amadeus.com/v1/security/oauth2/token"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
@@ -21,18 +27,18 @@ def getAccessToken():
     if response.status_code == 200:
         token_data = response.json()
         access_token = token_data['access_token']
-        token_expiry = time.time() + 29*60  
+        expiry = time.time() + 29*60  
         return access_token
     else:
         raise Exception("Failed to obtain access token")
 
 def getValidAccessToken():
-    global access_token, token_expiry
-    if access_token is None or time.time() >= token_expiry:
+    global access_token, expiry
+    if access_token is None or time.time() >= expiry:
         return getAccessToken()
     return access_token
 
-def getCheapestFlight(data):
+def getCheapestFlightData(data):
     price = float(data['data'][0]['price']['total'])
     cheapest = data['data'][0]
     for i in data['data']:
@@ -41,7 +47,7 @@ def getCheapestFlight(data):
             cheapest = i
     return cheapest
 
-def getFlightsData(origin, destination, date, ):
+def getFlightsData(origin, destination, date):
     url = f"https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode={origin}&destinationLocationCode={destination}&departureDate={date}&currencyCode=USD&adults=1"
     try:
         token = getValidAccessToken()  
@@ -58,3 +64,30 @@ def getFlightsData(origin, destination, date, ):
     except Exception as e:
         print('Error:', e)
         return None
+
+def getCheapestFlight(origin, destination, date,nocache=False):
+    cacheKey = f"cheapest-{origin}-{destination}-{date}"
+    if(nocache):
+        cached_data = redisClient.get(cacheKey)
+        print("Getting data from cache")
+        if cached_data:
+            return json.loads(cached_data)
+    print("Getting data from API") 
+    data = getFlightsData(origin,destination,date)
+    if(data):
+        cheapest = getCheapestFlightData(data)
+        price =int(float(cheapest['price']['total']))
+        data = {
+        "origin": origin,
+        "destination": destination,
+        "departure_date": date,
+        "price": f"{price} USD"
+        }
+        redisClient.set(cacheKey, json.dumps(data), ex=CACHE_EXPIRY)
+        return {
+        "data":data
+        }
+    return {
+        "message": "No flights found"
+    }
+    
